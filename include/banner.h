e65aa73eba626760c79f56680837f51645e6c1d4
hustavojhon <hustavojhon@gmail.com>
2025-10-20 19:12:33 -0700

    feat: create basic banner and menu headers

---
+++ include/banner.h
@@ -0,0 +1,10 @@
+#ifndef BANNER_H
+#define BANNER_H
+
+// banner.h
+// Declaración de la función que muestra el banner ASCII.
+// No usa librerías adicionales: implementación en src/banner.cpp
+
+void mostrarBanner();
+
+#endif // BANNER_H
---
+++ include/menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+// menu.h
+// Declaración de la función que muestra el menú principal.
+
+void mostrarMenu();
+
+#endif // MENU_H
